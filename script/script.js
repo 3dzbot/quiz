4e74dd2f5f9fc218619e2176f332587e43ab63cf
3dzbot <zoduaks@ukr.net>
2020-05-17 19:51:32 +0300

    lessons3

--- script/script.js
+++ script/script.js
@@ -8,18 +8,9 @@ document.addEventListener('DOMContentLoaded', function(){
         burgerBtn = document.getElementById('burger'),
         prevButton = document.getElementById('prev'),
         nextButton = document.getElementById('next'),
-        res = [
-          {
-            img: './image/burger.png',
-            name: 'Стандарт'
-          },
-          {
-            img: './image/burgerBlack.png',
-            name: 'Черный'
-          }  
-        ];
-
-        const questions = [
+        sendButton = document.getElementById('send'),
+        modalDialog = document.querySelector('.modal-dialog'),
+        questions = [
           {
               question: "Какого цвета бургер?",
               answers: [
@@ -93,52 +84,116 @@ document.addEventListener('DOMContentLoaded', function(){
               type: 'radio'
           }
       ];
-      
-      // const obj = {}
-      
-      // const getData = () => {
-      //     formAnswers.textContent = 'LOAD';
-      
-      //     setTimeout(() => {
-      //         fetch('http://localhost:81/Quiz__intens/db.json')
-      //             .then(res => res.json())
-      //             .then(obj => playTest(obj.questions))
-      //     }, 2000);
-      // }
-      
-      // const obj = {};
-      
-      // const inputs = [...formAnswers.elements]
-      //     .filter(elem => elem.checked)
-      
-      // inputs.forEach((elem, index) => {
-      //     obj[`${index}_${questions[numberQuestion].question}`] = elem.value;
-      // })
-      // finalAnswers.push(obj)
-      
 
+      let clientWidth = document.documentElement.clientWidth;
+      
+      burgerBtn.style.display = 'none';  
 
+      if(clientWidth <= 768) {
+        burgerBtn.style.display = 'flex';  
+      } else {
+        burgerBtn.style.display = 'none';
+      }
+
+    const playTest = ()=>{
+
+    let numberQuestion = 0; 
+    const finalAnswers = [];
+
+    const renderAnswers = function(index){
+      questions[index].answers.forEach((answer) => {
+        const answerItem = document.createElement('div');
+        answerItem.classList.add('answers-item', 'd-flex', 'justify-content-center');
+        answerItem.innerHTML = `
+          <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}">
+          <label for="${answer.title}" class="d-flex flex-column  justify-content-between">
+            <img class="answerImg" src="${answer.url}" alt="burger">
+            <span>${answer.title}</span>
+          </label>
+        `;
+        formAnswers.appendChild(answerItem);
+      })
+    };
+
+    const renderQuestions = (indexQuestion)=>{
+      formAnswers.innerHTML = '';
+
+      if(numberQuestion >= 0 && numberQuestion <= questions.length - 1){
+        questionTitle.textContent = `${questions[indexQuestion].question}`;
+        renderAnswers(indexQuestion);
+        sendButton.classList.add('d-none');
+        nextButton.classList.remove('d-none');
+        prevButton.classList.remove('d-none');
+      }
+      if(numberQuestion === questions.length){
+        formAnswers.textContent = 'Спасибо.'
+        nextButton.classList.add('d-none');
+        prevButton.classList.add('d-none');
+        sendButton.classList.remove('d-none');
+
+        formAnswers.innerHTML = `
+        <div class="form-group">
+          <label for="numberPhone">Enter your phone</label>
+          <input type="number" class="form-control" id="numberPhone">
+        </div>
+        `;
+      } else if (numberQuestion <= 0){
+        prevButton.classList.add('d-none');
+      } else if (numberQuestion === questions.length + 1){
+        formAnswers.textContent = 'Спасибо за пройденный тест!';
+        setTimeout(()=>{
+          modalBlock.classList.remove('d-block');
+        }, 1000);
+      }
+    }
+    renderQuestions(numberQuestion);
+
+    const checkAnswer = function(){
+      const obj = {};
+      
+      const inputs = [...formAnswers.elements].filter((input) =>input.checked || input.id === 'numberPhone');
+      inputs.forEach((input, index) => {
+        if(numberQuestion >= 0 && numberQuestion <= questions.length - 1){
+          obj[`${index+1}. ${questions[numberQuestion].question}`] = input.value;
+        } else if (numberQuestion === questions.length){
+          obj['Номер телефона'] = input.value;
+        }
+      })
+      
+      finalAnswers.push(obj);
+     };
+
+    nextButton.addEventListener('click', ()=>{
+      checkAnswer();
+      numberQuestion++;
+      renderQuestions(numberQuestion);
+    })
+
+    prevButton.addEventListener('click', ()=>{
+      numberQuestion--;
+      renderQuestions(numberQuestion);
+    })
+
+    sendButton.addEventListener('click', ()=>{
+      checkAnswer();
+      numberQuestion++;
+      renderQuestions(numberQuestion);
+      console.log(finalAnswers);
+    })
+  }
 
   btnOpenModal.addEventListener('click', ()=>{
     modalBlock.classList.add('d-block');
+    interval = requestAnimationFrame(animateModal);
     playTest();
   })
-
-  burgerBtn.style.display = 'none';  
 
   burgerBtn.addEventListener('click', ()=>{
     burgerBtn.classList.toggle('active');
     modalBlock.classList.add('d-block');
+    interval = requestAnimationFrame(animateModal);
     playTest();
   })
-
-  let clientWidth = document.documentElement.clientWidth;
-
-  if(clientWidth <= 768) {
-    burgerBtn.style.display = 'flex';  
-  } else {
-    burgerBtn.style.display = 'none';
-  }
 
   modalWrap.addEventListener('click', function(e){
     let target = e.target;
@@ -163,47 +218,19 @@ document.addEventListener('DOMContentLoaded', function(){
     burgerBtn.classList.toggle('active');
   })
 
-  const playTest = ()=>{
-    let numberQuestion = 0; 
+  let count = -100;
+  let interval;
 
-    const renderAnswers = function(index){
-      questions[index].answers.forEach((answer) => {
-        const answerItem = document.createElement('div');
-        answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
-        answerItem.innerHTML = `
-          <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
-          <label for="${answer.title}" class="d-flex flex-column  justify-content-between">
-            <img class="answerImg" src="${answer.url}" alt="burger">
-            <span>${answer.title}</span>
-          </label>
-        `;
-        formAnswers.appendChild(answerItem);
-        if(numberQuestion <=0){
-          prevButton.style.display = 'none';
-        } else if (numberQuestion >= questions.length-1){
-          nextButton.style.display = 'none';
-        } else {
-          prevButton.style.display = '';
-          nextButton.style.display = '';
-        }
-      })
-    };
+  modalDialog.style.top = count + '%';
 
-    const renderQuestions = (indexQuestion)=>{
-      formAnswers.innerHTML = '';
-      questionTitle.textContent = `${questions[indexQuestion].question}`;
-      renderAnswers(indexQuestion);
+  const animateModal = ()=>{
+    modalDialog.style.top = count + '%';
+    count+=3;
+    interval = requestAnimationFrame(animateModal);
+
+    if(count >=0){
+      cancelAnimationFrame(interval);
+      count = -100;
     }
-    renderQuestions(numberQuestion);
-
-    nextButton.addEventListener('click', ()=>{
-      numberQuestion++;
-      renderQuestions(numberQuestion);
-    })
-
-    prevButton.addEventListener('click', ()=>{
-      numberQuestion--;
-      renderQuestions(numberQuestion);
-    })
-  }
+  };
 })
